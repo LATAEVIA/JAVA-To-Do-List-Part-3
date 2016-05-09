c40085cafad21648c6e50e6caa2c98c054236f65
LaTaevia Berry and Reed Lambier <lataevia@>
2016-05-09 16:47:51 -0700

    Add partly finished working extra features

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -84,9 +84,31 @@ public class App {
       return null;
     });
 
+    get("/categories/:category_id/tasks/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Category category = Category.find(Integer.parseInt(request.params(":category_id")));
+      Task task = Task.find(Integer.parseInt(request.params(":id")));
+      model.put("category", category);
+      model.put("task", task);
+      model.put("template", "templates/task.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/categories/:category_id/tasks/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Task task = Task.find(Integer.parseInt(request.params("id")));
+      String description = request.queryParams("description");
+      Category category = Category.find(Integer.parseInt(request.params(":category_id")));
+
+      //task object: task's id, description
+      //category via url param :category_id
+
+
+      task.update(description);
+      String url = String.format("/categories/%d/tasks/%d", category.getId(), task.getId());
+      response.redirect(url);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
   }
 }
-
-// INSERT INTO tasks (description) VALUES ('Epicodus homework') RETURNING id;
-// INSERT INTO tasks (description) VALUES ('cook dinner') RETURNING id;
-// INSERT INTO tasks (description) VALUES ('laundry') RETURNING id;
